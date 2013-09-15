df97df2e3ec4bb12b30ca936f92cb01d366f45ac
Michael Crosby <crosby.michael@gmail.com>
2013-09-15 20:45:00 +0000

    Update python code

--- messages-py/__init__.py
+++ messages-py/__init__.py
@@ -2,6 +2,17 @@ import os
 import redis
 from datetime import datetime as dt
 import json
+from hashlib import md5
+import random, string
+
+def _new_id(mailbox):
+    n = dt.now()
+    created = n.timestamp().__str__() 
+    h = md5() 
+    buf = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    h.write(buff)
+    h.write(created)
+    return ('message:%s:%s' % (h.hexdigext(), mailbox), created,)
 
 class Message(object):
     def __init__(self, id, created, mailbox):
@@ -22,6 +33,7 @@ class Mailbox(object):
     def __init__(self, name):
         """ Create a new mailbox with the name """
         self.name = name
+        self.default_wait_timeout = 0
         proto = os.environ.get('REDIS_PROTO', 'tcp')
         addr = os.environ.get('REDIS_ADDR', 'localhost:6379')
         p = addr.split(':')
@@ -32,7 +44,6 @@ class Mailbox(object):
 
     def send(self, m):
         """ Send the message """
-        pass
     
     def delete(self, m):
         """ Delete the message from the mailbox now """
@@ -40,11 +51,17 @@ class Mailbox(object):
 
     def delete_after(self, m, seconds):
         """ Delete the message from the mailbox after n seconds """
-        pass
+        conn = redis.Redis(connection_pool=self._pool)
+        if seconds == 0:
+            conn.delete(m.id)
+        else:
+            conn.expire(m.id, seconds)
     
     def new_message(self):
         """ Return a new message """
-        pass
+        id, created = _new_id(self.name)
+        return Message(id, created, self.name)
+
 
     def wait(self):
         """ Wait for new messages in the mailbox """
